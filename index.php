0e278ef7f47fbeb35ed86631c3df27318cd3a5ec
Josip Rajković <sirsir55@gmail.com>
2017-11-19 23:13:59 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>PGD Krašce</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+
+</head>
+<body>
+
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
